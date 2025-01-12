7edf1844db0739e3e5bb6a2abe82df6cef35c655
abda-torey <abdatorey@gmail.com>
2025-01-12 03:20:09 +0000

    first push

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.15.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "taxi-data-447320"
+  region  = "europe-west2"
+}
+
+resource "google_storage_bucket" "bucket-demo" {
+  name          = "taxi-data-447320-bucket"
+  location      = "EU"
+  force_destroy = true
+
+ 
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
